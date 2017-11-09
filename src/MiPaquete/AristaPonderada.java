17b761b2107b4c6a35628a82bcdf35ca05fd2a53
Ezequiel <ezequiel.corno1996@gmail.com>
2017-11-09 20:50:13 -0300

    Se crean primeras clases

---
+++ src/MiPaquete/AristaPonderada.java
@@ -0,0 +1,31 @@
+package MiPaquete;
+
+public class AristaPonderada extends Arista {
+	
+	private int valor;
+	
+	public AristaPonderada(int nodo1, int nodo2, int valor) {
+		super(nodo1, nodo2);
+		this.setValor(valor);
+	}
+	
+	
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	@Override
+	public void mostrar() {
+		System.out.println(this.getNodo1() + " " + this.getNodo2() + " " + this.getValor());
+	}
+
+	
+	public int compareTo(AristaPonderada arista) {
+		return this.valor - arista.getValor();
+	}
+}
---
+++ src/MiPaquete/GrafoNDNP.java
@@ -0,0 +1,33 @@
+package MiPaquete;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class GrafoNDNP {
+	
+	private MatrizSimetrica grafo;
+	private int cantNodos;
+	private int cantAristas;
+	private double porcentajeAdyacencia;
+	private int gradoMax;
+	private int gradoMin;
+	
+	public GrafoNDNP(String path) throws FileNotFoundException {
+		
+		File file = new File(path);
+		Scanner scan = new Scanner(file);
+		int fil;
+		int col;
+		int indice;
+
+		this.cantNodos = scan.nextInt();
+		this.cantAristas = scan.nextInt();
+		this.porcentajeAdyacencia = scan.nextDouble();
+		this.gradoMax = scan.nextInt();
+		this.gradoMin = scan.nextInt();
+		this.grafo = new MatrizSimetrica(this.cantNodos);
+		
+		
+
+}
