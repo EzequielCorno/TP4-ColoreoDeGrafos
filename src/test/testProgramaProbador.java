50a70718d5caffc0e50bb807880426d141105a99
federicoSchafer <fedesmbp@gmail.com>
2017-11-21 01:59:17 -0300

    haciendo test

--- src/test/testProgramaProbador.java
+++ src/test/testProgramaProbador.java
@@ -12,9 +12,36 @@ import programaprobador.ProgramaProbador;
 public class testProgramaProbador {
 
 	@Test
-	public void testProgPobador() throws FileNotFoundException {
+	public void testProgPobadorNPartito() throws FileNotFoundException {
 		GrafoNDNP g1 = new GrafoNDNP("grafoNPartito.in");
-		ProgramaProbador p1 = new ProgramaProbador(g1.getGrafo(), "grafoColoreadoSecuencial.out");
+		ProgramaProbador p1 = new ProgramaProbador(g1.getGrafo(), "grafoColoreadoSecuencialNPartito.out");
+		if(p1.valido())
+			System.out.println("Esta bien coloreado.");
+		else System.out.println("Esta mal coloreado.");
+	}
+	
+	@Test
+	public void testProgPobadorAleaPorAdy() throws FileNotFoundException {
+		GrafoNDNP g1 = new GrafoNDNP("grafoAleatorioPorcentajeAdyacencia.in");
+		ProgramaProbador p1 = new ProgramaProbador(g1.getGrafo(), "grafoColoreadoSecuencialAleaPorAdy.out");
+		if(p1.valido())
+			System.out.println("Esta bien coloreado.");
+		else System.out.println("Esta mal coloreado.");
+	}
+	
+	@Test
+	public void testProgPobadorAleaProb() throws FileNotFoundException {
+		GrafoNDNP g1 = new GrafoNDNP("grafoAleatorioProbabilidad.in");
+		ProgramaProbador p1 = new ProgramaProbador(g1.getGrafo(), "grafoColoreadoSecuencialAleaProb.out");
+		if(p1.valido())
+			System.out.println("Esta bien coloreado.");
+		else System.out.println("Esta mal coloreado.");
+	}
+	
+	@Test
+	public void testProgPobadorRegPorAdy() throws FileNotFoundException {
+		GrafoNDNP g1 = new GrafoNDNP("grafoRegularPorAdyacencia.in");
+		ProgramaProbador p1 = new ProgramaProbador(g1.getGrafo(), "grafoColoreadoSecuencialRegPorAdy.out");
 		if(p1.valido())
 			System.out.println("Esta bien coloreado.");
 		else System.out.println("Esta mal coloreado.");
